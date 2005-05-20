de39e79e8938bb583787a2cc0365cd100704fa83
farpro <farpro>
2005-05-20 00:14:52 +0000

    Edicoes no makefile, leiame e no modulo de mapas

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -1,19 +1,4 @@
-/*   DCC Nightmare - A Free 3d Role Playing Game
- *   Copyright (C) 2005  Inconha Software LTDA.
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+/* DCC Nightmare is Public Domain - Do whatever you want with this code.
  */
 
 #include "map.h"
@@ -36,18 +21,18 @@ Square::~Square()
 	return;
 }
 
-int Square::draw( GLfloat x, GLfloat y )
+int Square::draw( GLfloat x, GLfloat z )
 {
 #ifdef DEBUG_MAP
-	printf("map.cpp - Square::draw(): Drawing square in (%f,%f)\n", x, y);
+	printf("map.cpp - Square::draw(): Drawing square in (%f,%f)\n", x, z);
 #endif
 //	glEnable(GL_TEXTURE_2D);
-	glColor3f( 0.0, 0.0, 0.0 );
+	glColor3f( 1.0, 1.0, 20.0 );
 	glBegin(GL_POLYGON);
-	glVertex3f( x + (SQUARESIZE/2), y + (SQUARESIZE/2), 0.0 );
-	glVertex3f( x - (SQUARESIZE/2), y + (SQUARESIZE/2), 0.0 );
-	glVertex3f( x - (SQUARESIZE/2), y - (SQUARESIZE/2), 0.0 );
-	glVertex3f( x + (SQUARESIZE/2), y - (SQUARESIZE/2), 0.0 );
+	glVertex3f( x + (SQUARESIZE/2), 0.0, z + (SQUARESIZE/2) );
+	glVertex3f( x - (SQUARESIZE/2), 0.0, z + (SQUARESIZE/2) );
+	glVertex3f( x - (SQUARESIZE/2), 0.0, z - (SQUARESIZE/2) );
+	glVertex3f( x + (SQUARESIZE/2), 0.0, z - (SQUARESIZE/2) );
 //	glNormal3i( 0, 0, 1 );
 	glEnd();
 	return(0);
@@ -55,60 +40,28 @@ int Square::draw( GLfloat x, GLfloat y )
 
 int Map::draw()
 {
-	Square * ref = center, * aux = center;
+	Square * ref = first;
+        Square * aux = first;
 	GLfloat x = 0.0, y = 0.0;
 #ifdef DEBUG_MAP
 	printf("map.cpp - Map::draw(): Drawing map %s.\n", name);
 #endif
-	while( ref != NULL )
+	while( aux != NULL )
 	{
-		while( aux != NULL )
-		{
-			(*aux).draw( x, y );
-			y += SQUARESIZE;
-			aux = (*aux).up;
-		}
-
-		y = -SQUARESIZE;
-		aux = (*ref).down;
-
-		while( aux != NULL )
-		{
-			(*aux).draw( x, y );
-			y -= SQUARESIZE;
-			aux = (*aux).down;
-		}
-		ref = (*ref).left;
-		aux = ref;
-		x -= SQUARESIZE; y = 0.0;
-	}
-
-	ref = (*center).right;
-	x = SQUARESIZE; y = 0.0;
-
-	while( ref != NULL )
-	{
-		while( aux != NULL )
-		{
-			(*aux).draw( x, y );
-			y += SQUARESIZE; 
-			aux = (*aux).up;
-		}
-
-		y = 0.0;
-		aux = ref;
-
-		while( aux != NULL )
-		{
-			(*aux).draw( x, y );
-			y -= SQUARESIZE;
-			aux = (*aux).down;
-		}
-		ref = (*ref).right;
-		aux = ref;
-		x += SQUARESIZE; y = 0.0;
-	}
-
+           (*aux).draw(x,y);
+           aux = aux->right;
+           if(aux == NULL)   //chegou ao fim da linha
+           {
+              aux = ref->down;
+              ref = ref->down;
+              y += SQUARESIZE;
+              x = 0.0;
+           } 
+           else      //senao continua na linha
+           {
+              x += SQUARESIZE;
+           } 
+        }
 	return(0);
 
 }
@@ -117,18 +70,18 @@ int Map::draw()
 
 Map::Map()
 {
-	center = NULL;
+	first = NULL;
 	name = NULL;
 }
 
-int Map::open()
+int Map::open(char* arquivo)
 {
-	center = new(Square);
+	/*first = new(Square);
 	(*center).up = new(Square);
 	(*center).down = new(Square);
 	(*center).left = new(Square);
 	(*center).right = new(Square);
-	name = "2014";
+	name = "2014";*/
 	return(0);
 }
 
--- src/map/teste.c
+++ src/map/teste.c
@@ -1,7 +1,7 @@
 #include "map.h"
 int main( int argc, char ** argv ){
 	Map * mapa = new(Map);
-	(*mapa).open();
-	(*mapa).run_test();
+	(*mapa).open("arquivo.map");
+/*	(*mapa).run_test();*/
 	return(0);
 }
