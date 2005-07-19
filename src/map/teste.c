cdebf1a9999de3d81acc3cf5fc2b4e6c7c1b68c8
farpro <farpro>
2005-07-19 13:04:40 +0000

    Portado para PicoMole Ruindows

--- src/map/teste.c
+++
@@ -1,8 +0,0 @@
-#include "map.h"
-int main( int argc, char ** argv ){
-	Map * mapa = new(Map);
-	(*mapa).open("../data/mapas/arquivo.map");
-/*	(*mapa).run_test();*/
-        delete(mapa);
-	return(0);
-}
