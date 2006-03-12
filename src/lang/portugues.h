639d8daecd8a4964de29b29edb9efb000e7ab590
farpro <farpro>
2006-03-12 00:19:05 +0000

    Arrumado sistema de linguagens para estar em arquivo, podendo ser mudada em execucao, ao inves de em compilacao no .h

---
+++ src/lang/portugues.h
@@ -0,0 +1,58 @@
+/* Constantes de Linguagem: Portugues */
+#ifndef _portugues_h
+#define _portugues_h
+
+
+#define VERSION                "0.0.2"
+
+//static char VERSION[5];
+
+
+#define TEXTURE_INFORMATION    "../data/texturas/infoPort.png"
+
+/* dccnit.cpp */
+#define LOAD_NPC               "Carregando NPC: %s"
+#define LOAD_PC                "Carregando Personagem: %s"
+#define LOAD_MAP               "Carregando Mapa: %s"
+#define LOAD_WINDOWS           "Colocando Janelas na Tela"
+#define LOAD_DONE              "Pronto!"
+
+#define OBJ_DOOR               "Porta"
+#define OBJ_NOTHING            "Nada"
+
+#define WINDOW_MAP             "Mapa"
+#define WINDOW_SHORTCUTS       "Atalhos"
+#define WINDOW_SHORTCUTS_HELP  "Pressione F1 para Ajuda"
+#define WINDOW_SHORTCUTS_FPS   "FPS:"
+
+/* initial.cpp */
+#define INITIAL_NEW         "Novo"
+#define INITIAL_CONTINUE    "Continuar"
+#define INITIAL_SAVE        "Salvar"
+#define INITIAL_LOAD        "Carregar"
+#define INITIAL_EXIT        "Sair"
+#define INITIAL_OPTIONS     "Opções"
+
+/* options.cpp */
+#define OPTIONS_TITLE        "Opções"
+#define OPTIONS_MUSIC_VOLUME "Volume da Música:"
+#define OPTIONS_SNDFX_VOLUME "Volume dos Efeitos:"
+
+/* skillwindow.cpp */
+#define SKILL_TITLE         "Perícias"
+#define SKILL_FREE_POINTS   "Pontos Livres:"
+#define SKILL_POINTS        "Pontos:"
+#define SKILL_COST          "Custo:"
+#define SKILL_CONFIRM       "Confirmar"
+#define SKILL_CANCEL        "Cancelar"
+
+/* DIRECTORYS */
+#define SKILLS_DIR          "../data/skills/Portugues/"
+
+/* Erros */
+#define ERROR_MAP_OUT       "Argh! Mapa fora dos limites!\n"
+#define ERROR_INIT_SDL      "Oxi! Não consegui iniciar o SDL!\n"
+
+
+#endif
+
