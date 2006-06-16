4557c02f84c550aa8445930498e29aa113f49e68
farpro <farpro>
2006-06-16 23:43:19 +0000

    Os Dados

--- src/classes/defs.h
+++ src/classes/defs.h
@@ -1,6 +1,40 @@
 #ifndef _defs_h
 #define _defs_h
 
+/******************************************************************
+ *                        DICE CONSTANTS                          *
+ ******************************************************************/
+#define DICE_D2    2
+#define DICE_D4    4
+#define DICE_D6    6
+#define DICE_D8    8
+#define DICE_D10  10
+#define DICE_D12  12
+#define DICE_D20  20
+/******************************************************************
+ *                           DICE STRUCT                          *
+ ******************************************************************/
+typedef struct _dice
+{
+   int diceID;           /* ID of Dice */
+   int numberOfDices;    /* Number of Dices */
+   int sumNumber;        /* Base Number to Sum */
+}dice;
+
+typedef struct _diceThing
+{
+   /* Note: the number is calculated by: 
+        baseDice + ((level-initialLevel)/aditionalLevels)*aditionalDice
+    */
+   dice baseDice;         /* Basic Dice */
+   dice aditionalDice;    /* Aditional Dice per Level */
+   int aditionalLevels;   /* Number of Levels per Aditional Dices */
+   int initialLevel;      /* Initial Level to use this */
+}diceThing;
+
+/******************************************************************
+ *                        CLASSES CONSTANTS                       *
+ ******************************************************************/
 #define CLASS_EADM          0
 #define CLASS_EBIOLOGIA     1
 #define CLASS_EDCC          2
@@ -11,8 +45,6 @@
 #define CLASS_EMEDICINA     7
 #define CLASS_EMUSICA       8
 #define CLASS_ETO           9
-
-
 #define STR_EADM          "EADM"
 #define STR_EBIOLOGIA     "EBIOLOGIA"
 #define STR_EDCC          "EDCC"
