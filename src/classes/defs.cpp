4077d2ce0be54eb97632e10c5cb53c3e7b4b9644
farpro <farpro>
2007-05-10 22:31:47 +0000

    arrumadas constantes. Retiradas outras e colocados fixme para corrigir depois a maneira de verificacao

--- src/classes/defs.cpp
+++ src/classes/defs.cpp
@@ -3,190 +3,48 @@
 int numberConstant(string buffer)
 {
    int ret = -1;
-   if(buffer.compare(STR_EADM) == 0)
-     ret = CLASS_EADM;
-   else if(buffer.compare(STR_EBIOLOGIA) == 0)
-     ret = CLASS_EBIOLOGIA;
-   else if(buffer.compare(STR_EDCC) == 0)
-     ret = CLASS_EDCC;
-   else if(buffer.compare(STR_EDFISICA) == 0)
-     ret = CLASS_EDFISICA;
-   else if(buffer.compare(STR_EMECANICA) == 0)
-     ret = CLASS_EMECANICA;
-   else if(buffer.compare(STR_EFILOSOFIA) == 0)
-     ret = CLASS_EFILOSOFIA;
-   else if(buffer.compare(STR_EFISIOTERAPIA) == 0)
-     ret = CLASS_EFISIOTERAPIA;
-   else if(buffer.compare(STR_EMEDICINA) == 0)
-     ret = CLASS_EMEDICINA;
-   else if(buffer.compare(STR_EMUSICA) == 0)
-     ret = CLASS_EMUSICA;
-   else if(buffer.compare(STR_ETO) == 0)
-     ret = CLASS_ETO;
 
-   else if(buffer.compare(RACE_STR_AUTISTA_ESTRANHO) == 0 ) 
-      ret = RACE_AUTISTA_ESTRANHO;
-   else if(buffer.compare(RACE_STR_BOY_PATY) == 0 )
-      ret = RACE_BOY_PATY;
-   else if(buffer.compare(RACE_STR_GOTICO) == 0 )
-      ret = RACE_GOTICO;
-   else if(buffer.compare(RACE_STR_HIPPIE) == 0 )
-      ret = RACE_HIPPIE;
-   else if(buffer.compare(RACE_STR_HUMANO) == 0 )
-      ret = RACE_HUMANO;
-   else if(buffer.compare(RACE_STR_LLAMA_HUMANA) == 0 )
-      ret = RACE_LLAMA_HUMANA;
-   else if(buffer.compare(RACE_STR_METALEIRO) == 0 )
-      ret = RACE_METALEIRO;
-   else if(buffer.compare(RACE_STR_MANIACO_SKINHEAD) == 0 )
-      ret = RACE_MANIACO_SKINHEAD;
-   else if(buffer.compare(RACE_STR_MUTANTE) == 0 )
-      ret = RACE_MUTANTE;
-
-   else if(buffer.compare(RACE_STR_RATAZANA) == 0 )
-      ret = RACE_RATAZANA;
-   else if(buffer.compare(RACE_STR_ENTE) == 0 )
-      ret = RACE_ENTE;
-   else if(buffer.compare(RACE_STR_AMEIVA) == 0 )
-      ret = RACE_AMEIVA;
-
-   else if(buffer.compare(SKILL_STR_ACROBACIAS) == 0)
-      ret = SKILL_ACROBACIAS;
-   else if(buffer.compare(SKILL_STR_ADESTRAR_BICHOS) == 0)
-      ret = SKILL_ADESTRAR_BICHOS;
-   else if(buffer.compare(SKILL_STR_ARROMBAR) == 0)
-      ret = SKILL_ARROMBAR;
-   else if(buffer.compare(SKILL_STR_ARTE_DO_ESCAPE) == 0)
-      ret = SKILL_ARTE_DO_ESCAPE;
-   else if(buffer.compare(SKILL_STR_BLEFAR) == 0)
-      ret = SKILL_BLEFAR;
-   else if(buffer.compare(SKILL_STR_BOVINICE) == 0)
-      ret = SKILL_BOVINICE;
-   else if(buffer.compare(SKILL_STR_CNH_SOFRIMENTO) == 0)
-      ret = SKILL_CNH_SOFRIMENTO;
-   else if(buffer.compare(SKILL_STR_CURAR) == 0)
-      ret = SKILL_CURAR;
-   else if(buffer.compare(SKILL_STR_DIPLOMACIA) == 0)
-      ret = SKILL_DIPLOMACIA;
-   else if(buffer.compare(SKILL_STR_DISCURSO_PROF) == 0)
-      ret = SKILL_DISCURSO_PROF;
-   else if(buffer.compare(SKILL_STR_DISFARCES) == 0)
-      ret = SKILL_DISFARCES;
-   else if(buffer.compare(SKILL_STR_EQUILIBRIO) == 0)
-      ret = SKILL_EQUILIBRIO;
-   else if(buffer.compare(SKILL_STR_ESCALAR) == 0)
-      ret = SKILL_ESCALAR;
-   else if(buffer.compare(SKILL_STR_ESCONDER) == 0)
-      ret = SKILL_ESCONDER;
-   else if(buffer.compare(SKILL_STR_FALAR_IDIOMA) == 0)
-      ret = SKILL_FALAR_IDIOMA;
-   else if(buffer.compare(SKILL_STR_FALSIFICAR) == 0)
-      ret = SKILL_FALSIFICAR;
-   else if(buffer.compare(SKILL_STR_FURTIVIDADE) == 0)
-      ret = SKILL_FURTIVIDADE;
-   else if(buffer.compare(SKILL_STR_INTIMIDAR) == 0)
-      ret = SKILL_INTIMIDAR;
-   else if(buffer.compare(SKILL_STR_MONTAR) == 0)
-      ret = SKILL_MONTAR;
-   else if(buffer.compare(SKILL_STR_NADAR) == 0)
-      ret = SKILL_NADAR;
-   else if(buffer.compare(SKILL_STR_OBSERVAR) == 0)
-      ret = SKILL_OBSERVAR;
-   else if(buffer.compare(SKILL_STR_OP_OBJ_ELETRICO) == 0)
-      ret = SKILL_OP_OBJ_ELETRICO;
-   else if(buffer.compare(SKILL_STR_OP_OBJ_MECANICO) == 0)
-      ret = SKILL_OP_OBJ_MECANICO;
-   else if(buffer.compare(SKILL_STR_OBTER_INFO) == 0)
-      ret = SKILL_OBTER_INFO;
-   else if(buffer.compare(SKILL_STR_OUVIR) == 0)
-      ret = SKILL_OUVIR;
-   else if(buffer.compare(SKILL_STR_PASSE_LIVRE) == 0)
-      ret = SKILL_PASSE_LIVRE;
-   else if(buffer.compare(SKILL_STR_PE_NO_SACO) == 0)
-      ret = SKILL_PE_NO_SACO;
-   else if(buffer.compare(SKILL_STR_PERFORMAR) == 0)
-      ret = SKILL_PERFORMAR;
-   else if(buffer.compare(SKILL_STR_PRESTIDIG) == 0)
-      ret = SKILL_PRESTIDIG;
-   else if(buffer.compare(SKILL_STR_ROUPAS_BIZARRAS) == 0)
-      ret = SKILL_ROUPAS_BIZARRAS;
-   else if(buffer.compare(SKILL_STR_SEM_TEMPO) == 0)
-      ret = SKILL_SEM_TEMPO;
-   else if(buffer.compare(SKILL_STR_CNH_TAG_MEDIC) == 0)
-      ret = SKILL_CNH_TAG_MEDIC;
-   else if(buffer.compare(SKILL_STR_TOCAR_INST) == 0)
-      ret = SKILL_TOCAR_INST;
-
-   else if(buffer.compare(ATT_STR_LEVEL) == 0)
-      ret = ATT_LEVEL;
-   else if(buffer.compare(ATT_STR_STRENGHT) == 0)
-      ret = ATT_STRENGHT;
-   else if(buffer.compare(ATT_STR_DEXTERY) == 0)
-      ret = ATT_DEXTERY;
-   else if(buffer.compare(ATT_STR_CONSTITUTION) == 0)
-      ret = ATT_CONSTITUTION;
-   else if(buffer.compare(ATT_STR_INTELIGENCY) == 0)
-      ret = ATT_INTELIGENCY;
-   else if(buffer.compare(ATT_STR_WISDOW) == 0)
-      ret = ATT_WISDOW;
-   else if(buffer.compare(ATT_STR_CHARISM) == 0)
-      ret = ATT_CHARISM;
-
-   else if(buffer.compare(TEND_STR_SL_LIBERTARIO) == 0)
-     ret = TEND_SL_LIBERTARIO;
-   else if(buffer.compare(TEND_STR_SL_CENTRISTA) == 0)
-     ret = TEND_SL_CENTRISTA;
-   else if(buffer.compare(TEND_STR_SL_CAPITALISTA) == 0)
-     ret = TEND_SL_CAPITALISTA;
-   else if(buffer.compare(TEND_STR_NEUTRO_FUNCIONAL) == 0)
-     ret = TEND_NEUTRO_FUNCIONAL;
-   else if(buffer.compare(TEND_STR_NEUTRO_CENTRISTA) == 0)
-     ret = TEND_NEUTRO_CENTRISTA;
-   else if(buffer.compare(TEND_STR_NEUTRO_CAOTICO) == 0)
-     ret = TEND_NEUTRO_CAOTICO;
-   else if(buffer.compare(TEND_STR_SP_CAPITALISTA) == 0)
-     ret = TEND_SP_CAPITALISTA;
-   else if(buffer.compare(TEND_STR_SP_NEUTRO) == 0)
-     ret = TEND_SP_NEUTRO;
-   else if(buffer.compare(TEND_STR_SP_CAOTICO) == 0)
-     ret = TEND_SP_CAOTICO;
-
-   else if(buffer.compare(STR_WORK_ON_ALL) == 0)
-      ret = WORK_ON_ALL;
-   else if(buffer.compare(STR_NONE) == 0)
-      ret = NONE;
-
-
+   //FIXME NO More!
    return(ret);
 }
 
 
 bool isSkill(int number)
 {
-   return( (number >= SKILL_ACROBACIAS) && (number <= SKILL_TOCAR_INST));
+   return( (number >= ATT_SKILL_FIRST) && (number <= ATT_SKILL_LAST));
 }
 
 bool isClass(int number)
 {
-   return( (number >= CLASS_EADM) && (number <= CLASS_ETO) );
+   //FIXME
+   return(false);
+   //return( (number >= CLASS_EADM) && (number <= CLASS_ETO) );
 }
 
 bool isTendency(int number)
 {
-   return( (number >= TEND_SL_LIBERTARIO) && (number <= TEND_SP_CAOTICO) );
+   //FIXME
+   //return( (number >= TEND_SL_LIBERTARIO) && (number <= TEND_SP_CAOTICO) );
+   return(false);
 }
 
 bool isRace(int number)
 {
-   return( (number >= RACE_AUTISTA_ESTRANHO) && (number <= RACE_AMEIVA) );
+   //FIXME
+   return(false);
+   //return( (number >= RACE_AUTISTA_ESTRANHO) && (number <= RACE_AMEIVA) );
 }
 
 bool isAttribute(int number)
 {
-   return( (number >= ATT_LEVEL) && (number <= ATT_CHARISM) );
+   //FIXME
+   return(false);
+   //return( (number >= ATT_LEVEL) && (number <= ATT_CHARISM) );
 }
 
 bool isVariable(int number)
 {
-   return( (number >= VAR_AUX1) && (number <= VAR_AUX6) );
+   //FIXME
+   return(false);
+   //return( (number >= VAR_AUX1) && (number <= VAR_AUX6) );
 }
--- src/classes/defs.h
+++ src/classes/defs.h
@@ -20,7 +20,7 @@ using namespace std;
 #define ATT_WISDOW       5
 #define ATT_CHARISM      6
 #define ATT_SKILL_FIRST  7
-#define ATT_SKILL_LAST   39
+#define ATT_SKILL_LAST   46
 
 #define ATT_STR_LEVEL        "ATT_LEVEL"
 #define ATT_STR_STRENGHT     "ATT_STRENGHT"
@@ -37,137 +37,6 @@ using namespace std;
 #define VAR_AUX4         43
 #define VAR_AUX5         44
 #define VAR_AUX6         45
-
-/******************************************************************
- *                         SKILL CONSTANTS                        *
- ******************************************************************/
-#define SKILL_ACROBACIAS       7
-#define SKILL_ADESTRAR_BICHOS  8
-#define SKILL_ARROMBAR         9
-#define SKILL_ARTE_DO_ESCAPE  10
-#define SKILL_BLEFAR          11
-#define SKILL_BOVINICE        12
-#define SKILL_CNH_SOFRIMENTO  13
-#define SKILL_CURAR           14
-#define SKILL_DIPLOMACIA      15
-#define SKILL_DISCURSO_PROF   16
-#define SKILL_DISFARCES       17
-#define SKILL_EQUILIBRIO      18
-#define SKILL_ESCALAR         19
-#define SKILL_ESCONDER        20
-#define SKILL_FALAR_IDIOMA    21
-#define SKILL_FALSIFICAR      22
-#define SKILL_FURTIVIDADE     23
-#define SKILL_INTIMIDAR       24
-#define SKILL_MONTAR          25
-#define SKILL_NADAR           26
-#define SKILL_OBSERVAR        27
-#define SKILL_OP_OBJ_ELETRICO 28
-#define SKILL_OP_OBJ_MECANICO 29
-#define SKILL_OBTER_INFO      30
-#define SKILL_OUVIR           31
-#define SKILL_PASSE_LIVRE     32
-#define SKILL_PE_NO_SACO      33
-#define SKILL_PERFORMAR       34
-#define SKILL_PRESTIDIG       35
-#define SKILL_ROUPAS_BIZARRAS 36
-#define SKILL_SEM_TEMPO       37
-#define SKILL_CNH_TAG_MEDIC   38
-#define SKILL_TOCAR_INST      39
-
-#define SKILL_STR_ACROBACIAS      "SKILL_ACROBACIAS" 
-#define SKILL_STR_ADESTRAR_BICHOS "SKILL_ADESTRAR_BICHOS"
-#define SKILL_STR_ARROMBAR        "SKILL_ARROMBAR"
-#define SKILL_STR_ARTE_DO_ESCAPE  "SKILL_ARTE_DO_ESCAPE"
-#define SKILL_STR_BLEFAR          "SKILL_BLEFAR"
-#define SKILL_STR_BOVINICE        "SKILL_BOVINICE"
-#define SKILL_STR_CNH_SOFRIMENTO  "SKILL_CNH_SOFRIMENTO"
-#define SKILL_STR_CURAR           "SKILL_CURAR"
-#define SKILL_STR_DIPLOMACIA      "SKILL_DIPLOMACIA"
-#define SKILL_STR_DISCURSO_PROF   "SKILL_DISCURSO_PROF"
-#define SKILL_STR_DISFARCES       "SKILL_DISFARCES"
-#define SKILL_STR_EQUILIBRIO      "SKILL_EQUILIBRIO"
-#define SKILL_STR_ESCALAR         "SKILL_ESCALAR"
-#define SKILL_STR_ESCONDER        "SKILL_ESCONDER"
-#define SKILL_STR_FALAR_IDIOMA    "SKILL_FALAR_IDIOMA"
-#define SKILL_STR_FALSIFICAR      "SKILL_FALSIFICAR"
-#define SKILL_STR_FURTIVIDADE     "SKILL_FURTIVIDADE"
-#define SKILL_STR_INTIMIDAR       "SKILL_INTIMIDAR"
-#define SKILL_STR_MONTAR          "SKILL_MONTAR"
-#define SKILL_STR_NADAR           "SKILL_NADAR"
-#define SKILL_STR_OBSERVAR        "SKILL_OBSERVAR"
-#define SKILL_STR_OP_OBJ_ELETRICO "SKILL_OP_OBJ_ELETRICO"
-#define SKILL_STR_OP_OBJ_MECANICO "SKILL_OP_OBJ_MECANICO"
-#define SKILL_STR_OBTER_INFO      "SKILL_OBTER_INFO"
-#define SKILL_STR_OUVIR           "SKILL_OUVIR"
-#define SKILL_STR_PASSE_LIVRE     "SKILL_PASSE_LIVRE"
-#define SKILL_STR_PE_NO_SACO      "SKILL_PE_NO_SACO"
-#define SKILL_STR_PERFORMAR       "SKILL_PERFORMAR"
-#define SKILL_STR_PRESTIDIG       "SKILL_PRESTIDIG"
-#define SKILL_STR_ROUPAS_BIZARRAS "SKILL_ROUPAS_BIZARRAS"
-#define SKILL_STR_SEM_TEMPO       "SKILL_SEM_TEMPO"
-#define SKILL_STR_CNH_TAG_MEDIC   "SKILL_CNH_TAG_MEDIC"
-#define SKILL_STR_TOCAR_INST      "SKILL_TOCAR_INST"
-
-/******************************************************************
- *                        CLASSES CONSTANTS                       *
- ******************************************************************/
-#define CLASS_EADM          300
-#define CLASS_EBIOLOGIA     301
-#define CLASS_EDCC          302
-#define CLASS_EDFISICA      303
-#define CLASS_EMECANICA     304
-#define CLASS_EFILOSOFIA    305
-#define CLASS_EFISIOTERAPIA 306
-#define CLASS_EMEDICINA     307
-#define CLASS_EMUSICA       308
-#define CLASS_ETO           309
-#define STR_EADM          "EADM"
-#define STR_EBIOLOGIA     "EBIOLOGIA"
-#define STR_EDCC          "EDCC"
-#define STR_EDFISICA      "EDFISICA"
-#define STR_EMECANICA     "EMECANICA"
-#define STR_EFILOSOFIA    "EFILOSOFIA"
-#define STR_EFISIOTERAPIA "EFISIOTERAPIA"
-#define STR_EMEDICINA     "EMEDICINA"
-#define STR_EMUSICA       "EMUSICA"
-#define STR_ETO           "ETO"
-
-/******************************************************************
- *                         PC RACES CONSTANTS                     *
- ******************************************************************/
-#define RACE_AUTISTA_ESTRANHO 100
-#define RACE_BOY_PATY         101
-#define RACE_GOTICO           102
-#define RACE_HIPPIE           103
-#define RACE_HUMANO           104
-#define RACE_LLAMA_HUMANA     105
-#define RACE_METALEIRO        106
-#define RACE_MANIACO_SKINHEAD 107
-#define RACE_MUTANTE          108
-
-#define RACE_STR_AUTISTA_ESTRANHO "RACE_AUTISTA_ESTRANHO"
-#define RACE_STR_BOY_PATY         "RACE_BOY_PATY"
-#define RACE_STR_GOTICO           "RACE_GOTICO"
-#define RACE_STR_HIPPIE           "RACE_HIPPIE"
-#define RACE_STR_HUMANO           "RACE_HUMANO"
-#define RACE_STR_LLAMA_HUMANA     "RACE_LLAMA_HUMANA"
-#define RACE_STR_METALEIRO        "RACE_METALEIRO"
-#define RACE_STR_MANIACO_SKINHEAD "RACE_MANIACO_SKINHEAD"
-#define RACE_STR_MUTANTE          "RACE_MUTANTE"
-
-
-/******************************************************************
- *                        NPC RACES CONSTANTS                     *
- ******************************************************************/
-#define RACE_RATAZANA         120
-#define RACE_ENTE             121
-#define RACE_AMEIVA           122
-
-#define RACE_STR_RATAZANA  "RACE_RATAZANA"
-#define RACE_STR_ENTE      "RACE_ENTE"
-#define RACE_STR_AMEIVA    "RACE_AMEIVA"
-
 
 /******************************************************************
  *                         TENDENCY CONSTANTS                     *
@@ -191,7 +60,6 @@ using namespace std;
 #define TEND_STR_SP_CAPITALISTA    "TEND_SP_CAPITALISTA"
 #define TEND_STR_SP_NEUTRO         "TEND_SP_NEUTRO"
 #define TEND_STR_SP_CAOTICO        "TEND_SP_CAOTICO"
-
 
 #define WORK_ON_ALL            10000
 #define STR_WORK_ON_ALL        "WORK_ON_ALL"
