1499958b7d5ac10c4e0423998b123aa89239627e
farpro <farpro>
2007-06-15 14:38:53 +0000

    doxygen doc

--- src/gui/messages.h
+++ src/gui/messages.h
@@ -7,12 +7,33 @@
 #include <string>
 using namespace std;
 
+/*! Get a string from user input
+ * \param title -> title of the window
+ * \param previous -> previous value of the string
+ * \param proj -> projection matrix
+ * \param modl -> model view matrix
+ * \param viewPort -> viewPort vector
+ * \return the value of the screen selected */
 string getStringFromUser(string title, string previous,
                          GLdouble proj[16],GLdouble modl[16],GLint viewPort[4]);
 
+/*! Get a Option from user input
+ * \param title -> title of the window
+ * \param opt1 -> Option 1
+ * \param opt2 -> Option 2
+ * \param proj -> projection matrix
+ * \param modl -> model view matrix
+ * \param viewPort -> viewPort vector
+ * \return the value of the option selected (1 or 2) */
 int getOptionFromUser(string title, string message, string opt1, string opt2,
                       GLdouble proj[16],GLdouble modl[16],GLint viewPort[4]);
 
+/*! Show a message to user
+ * \param title -> title of the window
+ * \param message -> message to show to user
+ * \param proj -> projection matrix
+ * \param modl -> model view matrix
+ * \param viewPort -> viewPort vector */
 void showMessage(string title, string message,
                  GLdouble proj[16],GLdouble modl[16],GLint viewPort[4]);
 
