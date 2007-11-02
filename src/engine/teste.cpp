776241661b47563534ec5d5d5ae92f0298eef758
farpro <farpro>
2007-11-02 13:58:17 +0000

    sync

--- src/engine/teste.cpp
+++ src/engine/teste.cpp
@@ -63,8 +63,7 @@ int main(int argc, char **argv)
           {
              glDisable(GL_LIGHTING);
              Engine->fadeOutTexture(tituloID,0,0,799,599);
-             Engine->LoadMap("../data/mapas/teste1.map",0);
-             //Engine->LoadMap("../data/mapas/tutorial/temple.map",0);
+             Engine->LoadMap("../data/mapas/tutorial/house1.map",0);
           }
        }
        
