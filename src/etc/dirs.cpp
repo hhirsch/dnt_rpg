d845010b9e222a5f9aa93a27ceb142ae2bad7896
farpro <farpro>
2008-01-21 18:00:18 +0000

    dirs

---
+++ src/etc/dirs.cpp
@@ -0,0 +1,51 @@
+#include "dirs.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef _MSC_VER
+   #include "../config_win.h"
+#else
+   #include "../config.h"
+#endif
+
+/*********************************************************************
+ *                         findDataDirectories                       *
+ *********************************************************************/
+void dirs::findDataDirectories()
+{
+   FILE* arq;
+   string file;
+
+   /* Try to open locally */
+   file = "../data/fnt/arial.ttf";
+   if((arq = fopen(file.c_str(),"r")))
+   {
+      fclose(arq);
+      dataPath = "../data/";
+   }
+   else
+   {
+      /* Try to open at defined datadir */
+      file = PACKAGE;
+      file += DATADIR;
+      file += "/fnt/arial.ttf";
+      if((arq = fopen(file.c_str(),"r")))
+      {
+         fclose(arq);
+      }
+      else
+      {
+         printf("Error: Couldn't define the data directories!\n");
+         exit(-3);
+      }
+   }
+
+}
+
+
+
+string dirs::dataPath = "";
+
+
+
---
+++ src/etc/dirs.h
@@ -0,0 +1,30 @@
+
+#ifndef _dirs_h
+#define _dirs_h
+
+#include <string>
+using namespace std;
+
+
+/*! The directory abstraction path */
+class dirs
+{
+   public:
+      /*! Find Where on the system data current is:
+       * (first try to find at ../data, if not found, isn´t running the local
+       *  state, so try at the DATADIR, if still not found, we must have an error) */
+      void findDataDirectories();
+
+      /*! Get the real path of an internal file path
+       * \param directory -> internal file path
+       * \return -> string with the real path of the file */
+      string getPath(string fileName);
+
+   protected:
+      static string dataPath; /**< Current Data Path */
+};
+
+
+#endif
+
+
