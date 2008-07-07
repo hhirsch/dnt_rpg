89a85d4e6cf807a8e0c14eb784a3ce505cfe7d89
farpro <farpro>
2008-07-07 22:06:09 +0000

    initing cbook implementation

---
+++ src/cbook/comicbook.h
@@ -0,0 +1,31 @@
+#ifndef _dnt_comicbook_h
+#define _dnt_comicbook_h
+
+/*! The comic book class is the DNT cutscene abstraction */
+class comicBook
+{
+   public:
+      /*! Constructor */
+      comicBook();
+      /*! Destructor */
+      ~comicBook();
+
+      /*! Load a comicBook from file
+       * \param bookFile -> comicBook file name 
+       * \return -> true if the load was successfull */
+      bool load(string bookFile);
+
+      /*! Run the comic book */
+      void run();
+
+   protected:
+
+      /*! Empty the book, deleting all its pages */
+      void empty();
+
+      comicPage* pages;   /**< Each comic book page  */
+      int totalPages;     /**< Total book pages */
+};
+
+#endif
+
---
+++ src/cbook/comicpage.h
@@ -0,0 +1,11 @@
+
+#ifndef _dnt_comic_page_h
+#define _dnt_comic_page_h
+
+
+class comicPage()
+
+
+#endif
+
+
