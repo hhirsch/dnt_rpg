07ab10700ad0798e60b42add590cba1084a98aaa
farpro <farpro>
2008-09-16 21:01:29 +0000

    fix for make install no more need to always update the makefile with new directories

--- install.sh
+++ install.sh
@@ -1,1 +1,41 @@
-#nothing for now.
+#!/bin/sh
+
+# Usage install.sh datadir package installApplication
+data=./data
+datadir=$1
+package=$2
+instApp=$3
+
+# Create destiny data directory
+$instApp -d $datadir/$package
+
+# Install All Data
+for directory in $(find $data -type d ); do
+  
+   # Ignore CVS directories
+   if [[ -z $(echo $directory | grep -i CVS) ]]; then
+     
+      
+      # Ignore the ./data directory creation itself
+      if [ "$directory" != "$data" ]; then
+         # remove the ./data/
+         dir=$(echo $directory | sed 's/\.\/data\///g')
+
+         # install the directory
+         $instApp -d $datadir/$package/$dir
+      else
+         dir=""
+      fi
+      
+      # Get and Install all files in the directory
+      for f in $data/$dir/*.*; do
+         if [ "$f" != "$data/$dir/*.*" ]; then
+            # install the file
+            $instApp -c -m 644 $f $datadir/$package/$dir
+         fi
+      done
+
+   fi
+
+done
+
