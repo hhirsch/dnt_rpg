82b8c0999c9fd1178685e5e7b1cead1aa4829a29
farpro <farpro>
2008-12-01 12:25:51 +0000

    loasind and saving iaStack to file

--- src/ia/iastack.cpp
+++ src/ia/iastack.cpp
@@ -1,5 +1,11 @@
 #include "iastack.h"
 
+#define IA_STACK_TOKEN_BEGIN         "begin"
+#define IA_STACK_TOKEN_END           "end"
+#define IA_STACK_TOKEN_LINE_NUMBER   "lineNumber"
+#define IA_STACK_TOKEN_COMMAND       "command"
+
+#define IA_STACK_TOKEN_END_STACK     "endStack"
 
 /***********************************************************
  *                       Constructor                       *
@@ -7,6 +13,7 @@
 iaStack::iaStack()
 {
    top = NULL;
+   bottom = NULL;
    total = 0;
 }
 
@@ -21,6 +28,55 @@ iaStack::~iaStack()
       delete(jmp);
       jmp = pop();
    }
+}
+
+/***********************************************************
+ *                            pop                          *
+ ***********************************************************/
+iaJumpPos* iaStack::pop()
+{
+   iaJumpPos* jmp = top;
+
+   /* Update bottom pointer */
+   if(top == bottom)
+   {
+      bottom = NULL;
+   }
+
+   /* Update Top Pointer */
+   if(top)
+   {
+      top = top->previous;
+   }
+   else
+   {
+      top = NULL;
+   }
+
+   /* Update Total Counter */
+   if(total > 0)
+   {
+      total--;
+   }
+
+   /* Return the top */
+   return(jmp);
+}
+
+/***********************************************************
+ *                           push                          *
+ ***********************************************************/
+void iaStack::push(iaJumpPos* jmp)
+{
+   jmp->previous = top;
+   top = jmp;
+
+   if(bottom == NULL)
+   {
+      bottom = jmp;
+   }
+
+   total++;
 }
 
 /***********************************************************
@@ -39,36 +95,112 @@ void iaStack::print()
 }
 
 /***********************************************************
- *                            pop                          *
+ *                       insertAtBottom                    *
  ***********************************************************/
-iaJumpPos* iaStack::pop()
+void iaStack::insertAtBottom(iaJumpPos* jmp)
 {
-   iaJumpPos* jmp = top;
-
-   if(top)
+   /* Set the top, if needed */
+   if(total == 0)
    {
-      top = top->previous;
-   }
-   else
-   {
-      top = NULL;
+      top = jmp;
    }
 
-   if(total > 0)
+   /* Set the previous, if needed */
+   if(bottom)
    {
-      total--;
+      bottom->previous = jmp;
    }
 
-   return(jmp);
+   /* Insert at bottom and inc total */
+   bottom = jmp;
+   total++;
 }
 
 /***********************************************************
- *                           push                          *
+ *                          save                           *
  ***********************************************************/
-void iaStack::push(iaJumpPos* jmp)
+void iaStack::save(ofstream* file)
 {
-   jmp->previous = top;
-   top = jmp;
-   total++;
+   int i;
+   iaJumpPos* jmp = top;
+   for(i=0; i < total; i++)
+   {
+      *file << IA_STACK_TOKEN_BEGIN << " = " << jmp->begin << endl;
+      *file << IA_STACK_TOKEN_END << " = " << jmp->end << endl;
+      *file << IA_STACK_TOKEN_LINE_NUMBER << " = " << jmp->lineNumber << endl;
+      *file << IA_STACK_TOKEN_COMMAND << " = " << jmp->command << endl;
+
+      jmp = jmp->previous;
+   }
+   *file << IA_STACK_TOKEN_END_STACK << " = " << "END" << endl;
+}
+
+/***********************************************************
+ *                          load                           *
+ ***********************************************************/
+void iaStack::load(defParser* def)
+{
+   string value="", key="";
+   iaJumpPos* jmp = NULL;
+   int pos=0;
+
+   while(def->getNextTuple(key, value))
+   {
+      if(key == IA_STACK_TOKEN_END_STACK)
+      {
+         /* Done with the stack */
+         return;
+      }
+      else if(key == IA_STACK_TOKEN_BEGIN)
+      {
+         /* The begin position of a jump. It also mark the
+          * local to put a new jump on the stack */
+         jmp = new iaJumpPos();
+         insertAtBottom(jmp);
+         sscanf(value.c_str(),"%d",&pos);
+         jmp->begin = pos;
+      }
+      else if(key == IA_STACK_TOKEN_END)
+      {
+         /* The end position of a jump */
+         if(jmp)
+         {
+            sscanf(value.c_str(),"%d",&pos);
+            jmp->end = pos;
+         }
+         else
+         {
+            cerr << "Warning: No iaJumpPos for jump at loading stack!" << endl;
+         }
+      }
+      else if(key == IA_STACK_TOKEN_LINE_NUMBER)
+      {
+         /* The line Number of the jump */
+         if(jmp)
+         {
+            sscanf(value.c_str(),"%d",&jmp->lineNumber);
+         }
+         else
+         {
+            cerr << "Warning: No iaJumpPos for jump at loading stack!" << endl;
+         }
+      }
+      else if(key == IA_STACK_TOKEN_COMMAND)
+      {
+         /* The jump command */
+         if(jmp)
+         {
+            jmp->command = value;
+         }
+         else
+         {
+            cerr << "Warning: No iaJumpPos for jump at loading stack!" << endl;
+         }
+      }
+      else
+      {
+         cerr << "Warning: Unknow token '" << key << "' at stack load!"<< endl;
+      }
+   }
 }
 
--- src/ia/iastack.h
+++ src/ia/iastack.h
@@ -7,6 +7,8 @@
 #include <iostream>
 using namespace std;
 
+#include "../etc/defparser.h"
+
 /*! The jumps positions for if, while, for, etc. */
 class iaJumpPos
 {
@@ -39,8 +41,23 @@ class iaStack
       /*! Print the stack to the cerr for debug */
       void print();
 
+      /*! Save the Stack to a file
+       * \param file -> ofstream file to save to */
+      void save(ofstream* file);
+
+      /*! Load the defparser from a file
+       * \param def -> defParser to load from */
+      void load(defParser* def);
+
+   protected:
+      /*! Insert a iaJumpPos at the stack's bottom
+       * \param jmp -> jmp to be at bottom 
+       * \note -> usually used at stack load from file */
+      void insertAtBottom(iaJumpPos* jmp);
+
    private:
       iaJumpPos* top;      /**< The pointer to the top of the stack */
+      iaJumpPos* bottom;   /**< The pointer to the bottom of the stack */
       int total;           /**< The total of the stack */
 };
 
