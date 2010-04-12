b7b47734d151fa7cc3356f29da9275c4d3fc5cae
farpro <farpro@users.sourceforge.net>
2010-04-12 09:22:46 -0300

    "trying to avoid black screen at AmigaOS4

--- src/engine/initial.cpp
+++ src/engine/initial.cpp
@@ -95,6 +95,7 @@ int initialScreen::run(int Status,GLdouble proj[16],
    gui->openWindow(jan);
 
    glDisable(GL_LIGHTING);
+   glDisable(GL_FOG);
    glClearColor(0,0,0,1);
    glClear ((GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
