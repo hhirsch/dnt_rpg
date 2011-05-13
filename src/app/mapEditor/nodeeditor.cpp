c8dded65dc727e2bcf97ca546e632e787fcb68cd
farpro <farpro@users.sourceforge.net>
2011-05-13 16:44:07 -0300

    inited the implementation of sceneNode edit at mapEditor.

---
+++ src/app/mapEditor/nodeeditor.cpp
@@ -0,0 +1,54 @@
+/* 
+  DccNiTghtmare: a satirical post-apocalyptical RPG.
+  Copyright (C) 2005-2011 DNTeam <dnt@dnteam.org>
+ 
+  This file is part of DccNiTghtmare.
+ 
+  DccNiTghtmare is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DccNiTghtmare is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DccNiTghtmare.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "nodeeditor.h"
+
+
+/***********************************************************************
+ *                            Constructor                              *
+ ***********************************************************************/
+nodeEditor::nodeEditor()
+{
+}
+
+/***********************************************************************
+ *                              Destructor                             *
+ ***********************************************************************/
+nodeEditor::~nodeEditor()
+{
+}
+
+/***********************************************************************
+ *                             verifyAction                            *
+ ***********************************************************************/
+void nodeEditor::verifyAction(Uint8* keys, GLfloat mouseX, GLfloat mouseY, 
+            GLfloat mouseZ, Uint8 mButton, int mouseXw, int mouseYw,
+            GLdouble proj[16],GLdouble modl[16],GLint viewPort[4])
+{
+   Square* relSquare = actualMap->relativeSquareWorld(mouseX, mouseZ);
+
+   /* Create a bounding box for the mouse position */
+   boundingBox mouseBox;
+   mouseBox.setMin(mouseX-4, mouseY-4.0, mouseZ-4);
+   mouseBox.setMax(mouseX+4, mouseY+4.0, mouseZ+4);
+
+}
+
+
---
+++ src/app/mapEditor/nodeeditor.h
@@ -0,0 +1,65 @@
+/* 
+  DccNiTghtmare: a satirical post-apocalyptical RPG.
+  Copyright (C) 2005-2011 DNTeam <dnt@dnteam.org>
+ 
+  This file is part of DccNiTghtmare.
+ 
+  DccNiTghtmare is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DccNiTghtmare is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DccNiTghtmare.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _dnt_map_editor_node_editor_h
+#define _dnt_map_editor_node_editor_h
+
+#include <SDL/SDL_opengl.h>
+#include <SDL/SDL.h>
+
+#include "../../etc/scenenode.h"
+#include "../../map/map.h"
+
+/*! An editor to a scene node */
+class nodeEditor
+{
+   public:
+      /*! Constructor */
+      nodeEditor();
+
+      /*! Destructor */
+      ~nodeEditor();
+
+      /*! Select a node to edit (unselecting any previous one)
+       * \param scNode -> sceneNode to edit */
+      void selectNode(sceneNode* scNode);
+
+      /*! Verify and do action by mouse and state 
+       * \param keys -> current keyboard state
+       * \param mouseX -> mouse X coordinate
+       * \param mouseY -> mouse Y coordinate 
+       * \param mouseZ -> mouse Z coordinate 
+       * \param mouseXw -> x window mouse coordinate
+       * \param mouseYw -> y window mouse coordinate
+       * \param mButton -> mouse button state
+       * \param proj -> projection matrix
+       * \param modl -> modelview matrix
+       * \param viewPort -> viewport matrix */
+      void verifyAction(Uint8* keys, GLfloat mouseX, GLfloat mouseY, 
+            GLfloat mouseZ, Uint8 mButton, int mouseXw, int mouseYw,
+            GLdouble proj[16],GLdouble modl[16],GLint viewPort[4]);
+
+   protected:
+
+      sceneNode* curNode;  /**< current node to edit */
+};
+
+#endif
+
