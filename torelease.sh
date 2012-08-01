ce9ab4098fe9936419a84e92154f3ae007846a7d
farpro <farpro@users.sourceforge.net>
2012-08-01 16:21:39 -0300

    added GPL disclaimer to script.

--- torelease.sh
+++ torelease.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+#  DNT: a satirical post-apocalyptical RPG.
+#  Copyright (C) 2005-2012 DNTeam <dnt@dnteam.org>
+# 
+#  This file is part of DNT.
+# 
+#  DNT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  DNT is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with DNT.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # torelease.sh will create a dnt tar.bz2 with the needed files
 # for a source release.
 
